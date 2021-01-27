0c0d83023e8a69b0bc63a8a6970c6033db9b1d2d
sourav chatterjee <sourav.chatterjee@mindfiresolutions.com>
2021-01-27 12:13:56 +0530

    boilerplate setup done

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Product from './Product'
+import '../assets/styles/index.css'
+
+const Container = () => {
+  return (
+    <>
+      <Header/>
+      <Product/>
+    </>
+  )
+}
+
+export default Container
