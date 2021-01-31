0e067128e7e216c07566c48ede6a3297a0763574
sourav chatterjee <sourav.chatterjee@mindfiresolutions.com>
2021-01-31 15:20:48 +0530

    added loader skeleton

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import { useImmer } from "use-immer"
 
 import Product from './Product'
+import ProductSkeleton from './ProductSkeleton'
+
 import { getProducts } from '../services/Product'
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -43,10 +45,30 @@ const ProductList = (props:any) => {
     // eslint-disable-next-line
   },[])
 
+  const productLoader = useMemo(()=>(
+    <>
+      <Grid item xs={12} sm={6} md={3}>
+        <ProductSkeleton />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <ProductSkeleton />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <ProductSkeleton />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <ProductSkeleton />
+      </Grid>
+    </>
+  ),[])
+
   return (
     <Container maxWidth="md">
       <div className={classes.root}>
         <Grid container spacing={3} className={classes.listContainer}>
+          {!state.list.length &&
+            productLoader
+          }
           {state.list.map((value:any,index:number)=>(
             <Grid key={index} item xs={12} sm={6} md={3}>
               <Product key={index} data={value} {...props}/>
